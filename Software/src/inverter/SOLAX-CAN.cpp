13f25d0207b4cc2608e84b650bb2a4e750d79da7
Jonny <jonny@jonnybarker.com>
2025-08-07 19:28:45 +0100

    Required MG HS PHEV hacks

--- Software/src/inverter/SOLAX-CAN.cpp
+++ Software/src/inverter/SOLAX-CAN.cpp
@@ -212,7 +212,8 @@ void SolaxInverter::map_can_frame_to_variable(CAN_frame rx_frame) {
     logging.println("1871 05-frame received from inverter");
   }
   if (rx_frame.ID == 0x1871 && rx_frame.data.u8[0] == (0x03)) {
-    logging.println("1871 03-frame received from inverter");
+    // This appears to be a timestamp, that the inverter will send to certain
+    // models of battery. We are not interested in it.
   }
 }
 
