941c1f0a432bfa961cf590a7d770a6c1f35a1436
Jonny <jonny@jonnybarker.com>
2026-08-04 22:18:50 +0100

    MG4 battery integration

--- Software/src/devboard/webserver/settings_html.cpp
+++ Software/src/devboard/webserver/settings_html.cpp
@@ -1455,7 +1455,8 @@ const char* getCANInterfaceName(CAN_Interface interface) {
     form[data-shunttype="3"] .if-ctclamp { display: contents;}
     
 
-    form .if-cbms { display: none; }
+    form .if-cbms,
+    form .if-cv { display: none; }
     form[data-battery="6"] .if-cbms,
     form[data-battery="11"] .if-cbms,
     form[data-battery="22"] .if-cbms,
@@ -1465,7 +1466,8 @@ const char* getCANInterfaceName(CAN_Interface interface) {
     form[data-battery="41"] .if-cbms,
     form[data-battery="48"] .if-cbms,
     form[data-battery="49"] .if-cbms,
-    form[data-battery="51"] .if-cbms {
+    form[data-battery="51"] .if-cbms,
+    form[data-battery="55"] .if-cv {
       display: contents;
     }
 
@@ -1514,7 +1516,8 @@ const char* getCANInterfaceName(CAN_Interface interface) {
     form[data-battery="16"] .if-socestimated,
     form[data-battery="26"] .if-socestimated,
     form[data-battery="41"] .if-socestimated,
-    form[data-battery="42"] .if-socestimated {
+    form[data-battery="42"] .if-socestimated,
+    form[data-battery="55"] .if-socestimated {
       display: contents;
     }
 
@@ -1703,7 +1706,7 @@ const char* getCANInterfaceName(CAN_Interface interface) {
   </script>
 
 <div style='background-color: #404E47; padding: 10px; margin-bottom: 10px; border-radius: 50px'>
-        <form action='saveSettings' method='post' onsubmit='return validateWebAuthPassword() && confirmBmsRestart()'>
+        <form action='saveSettings' method='post' onsubmit='handleSubmit(); return validateWebAuthPassword() && confirmBmsRestart()'>
 
         <div style='grid-column: span 2; text-align: center; padding-top: 10px;' class="%SAVEDCLASS%">
           <p>Settings saved. Reboot to take the new settings into use.<p> <button type='button' onclick='askReboot()'>Reboot</button>
@@ -1929,7 +1932,8 @@ const char* getCANInterfaceName(CAN_Interface interface) {
         <label>Battery min design voltage (V): </label>
         <input name='BATTPVMIN' pattern="[0-9]+(\.[0-9]+)?" type='text' value='%BATTPVMIN%' 
         title="Minimum safe voltage for the entire battery pack in volts. Further discharge not possible below this limit." />
-
+        </div>
+        <div class="if-cbms if-cv">
         <label>Cell max design voltage (mV): </label>
         <input name='BATTCVMAX' pattern="[0-9]+" type='text' value='%BATTCVMAX%' 
         title="Maximum voltage per individual cell in millivolts. Charging stops if one cell reaches this voltage." />
@@ -2387,6 +2391,13 @@ const char* getCANInterfaceName(CAN_Interface interface) {
         </div>
 
         </form>
+        <script>
+        function handleSubmit() {
+            document.querySelectorAll('input,select').forEach(input => {
+              input.disabled = input.offsetParent === null && input.type !== 'hidden';
+            });
+        }
+        </script>
     </div>
 
     <div style='background-color: #333; padding: 10px; margin-bottom: 10px; border-radius: 50px'>
