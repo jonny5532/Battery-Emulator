74d36e670cb6dec4040920e23371c5c6b0829679
Jonny <jonny@jonnybarker.com>
2026-08-05 13:06:27 +0100

    MG4: FD UDS

--- Software/src/battery/UdsCanBattery.h
+++ Software/src/battery/UdsCanBattery.h
@@ -207,6 +207,8 @@ class UdsCanBattery : public CanBattery, public IsoTp {
   uint32_t uds_response_address = 0;
   // The address we are currently receiving a UDS response from.
   uint32_t uds_current_response_address = 0;
+  // Whether UDS requests are sent as FD frames
+  bool fd_uds_requests = false;
 
  private:
   // True if the current pause blocks new sends of the given priority.
